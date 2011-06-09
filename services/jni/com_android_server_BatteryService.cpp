88434c32a84c259db21514afa2d1d0c7a4b723c5
gfunkmonk <gfunkmonk@gmail.com>
2011-06-09 15:30:08 +0800

    Revert "removed duplicate declaration of setPercentageField"
    This reverts commit 4316a0a8cbe3fc116d1bad886bacb823149b27ed.

--- services/jni/com_android_server_BatteryService.cpp
+++ services/jni/com_android_server_BatteryService.cpp
@@ -181,6 +181,25 @@ static void setIntField(JNIEnv* env, jobject obj, const char* path, jfieldID fie
     env->SetIntField(obj, fieldID, value);
 }
 
+static void setPercentageField(JNIEnv* env, jobject obj, const char* path, jfieldID fieldID)
+{
+    const int SIZE = 128;
+    char buf[SIZE];
+
+    jint value = 0;
+    if (readFromFile(path, buf, SIZE) > 0) {
+        value = atoi(buf);
+    }
+    /* sanity check for buggy drivers that provide bogus values, e.g. 103% */
+    if (value < 0) {
+        value = 0;
+    } else if (value > 100) {
+        value = 100;
+    }
+
+    env->SetIntField(obj, fieldID, value);
+}
+
 static void setVoltageField(JNIEnv* env, jobject obj, const char* path, jfieldID fieldID)
 {
     const int SIZE = 128;
